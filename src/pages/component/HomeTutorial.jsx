f4b5694e8cf67c7316d5234fbd7e00736c51f0d7
Arang King <jykim0827@gmail.com>
2024-06-14 17:03:07 +0900

    fixed some issues in home

--- src/pages/component/HomeTutorial.jsx
+++ src/pages/component/HomeTutorial.jsx
@@ -1,18 +1,19 @@
-
 import picture1 from "./assets/IMG_4264.jpg";
 import picture2 from "./assets/IMG_1.jpg";
 import picture3 from "./assets/IMG_2.jpg";
 import picture4 from "./assets/IMG_3.jpg";
 import picture5 from "./assets/IMG_4.jpg";
 import picture6 from "./assets/IMG_5.jpg";
-import picture7 from "./assets/IMG_6.jpg";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 
 const HomeTutorial = () => {
+  const navigate = useNavigate();
   return (
     <div className="home-tutorial">
       <div className="img-container">
         <p className="img-text">DEAR IMAGE, the easiest image edit tool</p>
-        <img src={picture1}/>
+        <img src={picture1} />
       </div>
       <div className="desc-text">
         <p>
@@ -21,7 +22,7 @@ const HomeTutorial = () => {
         </p>
       </div>
 
-      <div id="tutorial-colorpop">
+      <div id="tutorial-colorpop" className="tutorial-function">
         <p className="function-text">COLORPOP</p>
         <p className="tutorial-text">
           You can select the object to change its color to black and white.
@@ -40,7 +41,7 @@ const HomeTutorial = () => {
         <br />
       </div>
 
-      <div id="tutorial-replace">
+      <div id="tutorial-replace" className="tutorial-function">
         <p className="function-text">REPLACE</p>
         <p className="tutorial-text">
           You can select the object and replace it into another object. Please
@@ -59,7 +60,7 @@ const HomeTutorial = () => {
         <br />
       </div>
 
-      <div id="tutorial-emoji">
+      <div id="tutorial-emoji" className="tutorial-function">
         <p className="function-text">EMOJI</p>
         <p className="tutorial-text">
           You can add emoji by describing the position. Remember, the detailed
@@ -80,7 +81,7 @@ const HomeTutorial = () => {
         <br />
       </div>
 
-      <div id="tutorial-blur">
+      <div id="tutorial-blur" className="tutorial-function">
         <p className="function-text">BLUR</p>
         <p className="tutorial-text">
           You can blur the background of the object you selected. Please check
@@ -99,7 +100,7 @@ const HomeTutorial = () => {
         <br />
       </div>
 
-      <div id="tutorial-?">
+      <div id="tutorial-?" className="tutorial-function">
         <p className="function-text">GQA(OBJECT RECOGNITION)</p>
         <p className="tutorial-text">
           You can ask about the information of the image, such as the number of
@@ -121,6 +122,11 @@ const HomeTutorial = () => {
         <br />
         <br />
         <br />
+        <br />
+      </div>
+
+      <div id="start-button">
+        <Button text="Start Now" onClick={() => navigate("../Chat")} />
         <br />
         <br />
         <br />
