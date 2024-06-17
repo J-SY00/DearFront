b861ebdc46583085fe74e75899c3ab05ab96d305
jinsi123 <jin667000@gmail.com>
2024-06-18 01:13:12 +0900

    Reset Button

--- src/pages/component/ChatHeaderContainer.jsx
+++ src/pages/component/ChatHeaderContainer.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import Button from "./Button";
+import React from 'react';
+import Button from './Button';
 
-export default function HeaderContainer() {
+export default function HeaderContainer({ onResetChatSelected }) {
+  
+  const handleResetClick = () => {
+    const confirmed = window.confirm("정말로 리셋하시겠습니까?");
+    if (confirmed) {
+      onResetChatSelected(); //Chat에서 전달받은 함수 : 리셋버튼 작동됨
+    }
+  };
+
   return (
     <div className="chat-header">
-      <Button text="Reset"/>
+      <Button text="Reset" onClick={handleResetClick} />
     </div>
-  )
+  );
 }
-
