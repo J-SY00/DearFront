838cdf4ccda5528db54387259a88f277f700343a
jinsi123 <jin667000@gmail.com>
2024-06-29 12:29:14 +0900

    api image download

--- src/pages/component/api/DownloadAPI.js
+++ src/pages/component/api/DownloadAPI.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 //다운로드버튼
-export const getImageURL = () => {  
-  //예시(서버 답변 이미지 URL)
-  return axios.get('https://picsum.photos/800/800', {
+export const getImageURL = () => {
+  return axios.get(
+    // 'https://picsum.photos/800/800'
+    'https://imageURL', {
     responseType: 'blob',
   });
 };
